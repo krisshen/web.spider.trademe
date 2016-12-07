5803b918a8dbbc7edca05eb80cafe8b57dd7dc45
kris <kris1126@163.com>
2016-12-08 11:47:08 +1300

    page model class for cars

---
+++ src/main/groovy/spider/trademe/entity/Car.groovy
@@ -0,0 +1,15 @@
+package spider.trademe.entity
+
+import org.apache.http.annotation.ThreadSafe
+import us.codecraft.webmagic.model.annotation.HelpUrl
+import us.codecraft.webmagic.model.annotation.TargetUrl
+
+/**
+ * Created by shenk on 12/8/2016.
+ */
+@TargetUrl(value = "")
+@HelpUrl(value = "", sourceRegion = "")
+@ThreadSafe
+class Car {
+
+}
